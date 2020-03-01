02cb4ad89973f8c57f7e1e17659141595e488f47
yonggoonoh <nohyg90@gmail.com>
2020-03-01 16:34:38 +0900

    etc(-): Set up the basic webpack environment for frontend
    - webpack
    - babel
    - react

---
+++ frontend/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: "development",
+    entry: {
+        app: "./src/index.js"
+    },
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    devtool: "inline-source-map",
+    devServer: {
+        contentBase: "./dist"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            "@babel/preset-env",
+                            "@babel/preset-react"
+                        ],
+                        cacheDirectory: true // http://bit.ly/babel-cache
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        }),
+        new CleanWebpackPlugin({
+            verbose: true
+        })
+    ]
+};
